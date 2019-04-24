6ee411206722d63dbb81b885e9261518e74b9954
Nils van Doorn <nilsvandoorn@Nilss-MacBook-Air.local>
2019-04-24 11:54:57 +0200

    in progress paper1

--- eiwitten/option.py
+++ eiwitten/option.py
@@ -1,17 +1,99 @@
 from itertools import product
-"""Generates a list of all folding options for the protein"""
+
 class Option(object):
     def __init__(self, length):
         self.options = ["right", "left", "forward"]
 
-    def cluster(self, sequence, option):
-        length_sequence = len(sequence) - 3
-        i = 0
-        while i < length_sequence:
-            if sequence[i] == sequence[i+3] == "H":
-                if option[i] != option[i+1] or option[i] == "forward":
+    # finds all x,y amino positions of current option
+    def amino_positions(option):
+        positions = []
+        x, y = 10, 10
+        positions.append(tuple((x, y + 1)))
+        positions.append(tuple((x, y)))
+        direction = "d"
+        for move in option:
+            if direction == "d":
+                if move == "right":
+                    x = x - 1
+                    direction = "l"
+                elif move == "left":
+                    x = x + 1
+                    direction = "r"
+                elif move == "forward":
+                    y = y - 1
+            elif direction == "r":
+                if move == "right":
+                    y = y - 1
+                    direction = "d"
+                elif move == "left":
+                    y = y + 1
+                    direction = "u"
+                elif move == "forward":
+                    x = x + 1
+            elif direction == "l":
+                if move == "right":
+                    y = y + 1
+                    direction = "u"
+                elif move == "left":
+                    y = y - 1
+                    direction = "d"
+                elif move == "forward":
+                    x = x - 1
+            elif direction == "u":
+                if move == "right":
+                    x = x + 1
+                    direction = "r"
+                elif move == "left":
+                    x = x - 1
+                    direction = "l"
+                elif move == "forward":
+                    y = y + 1
+            positions.append(tuple((x, y)))
+        return positions
+
+    # determines if current fold option results in bumps
+    def viable_option(option):
+        number = 1
+        for position in option:
+            for i in range(len(option) - number):
+                if option[i + number] == position:
                     return False
-                else:
-                    i += 2
-            i += 1
+            number += 1
         return True
+
+    def fold_points(positions, sequence):
+        points = 0
+        HHHH = []
+        CCCC = []
+        for position, acid in zip(positions, sequence):
+            if acid == "H":
+                HHHH.append(position)
+            elif acid == "C":
+                CCCC.append(position)
+        for i in range(len(HHHH)):
+            if (HHHH[i][0] - 1, HHHH[i][1]) in (HHHH or CCCC):
+                points += 1
+            if (HHHH[i][0], HHHH[i][1] - 1) in (HHHH or CCCC):
+                points += 1
+        for i in range(len(CCCC)):
+            if (CCCC[i][0] - 1, CCCC[i][1]) in CCCC:
+                points += 5
+            elif (CCCC[i][0] - 1, CCCC[i][1]) in HHHH:
+                points += 1
+            if (CCCC[i][0], CCCC[i][1] - 1) in CCCC:
+                points += 5
+            elif (CCCC[i][0], CCCC[i][1] - 1) in HHHH:
+                points += 1
+        return points
+
+    # def cluster(self, sequence, option):
+    #     length_sequence = len(sequence) - 3
+    #     i = 0
+    #     while i < length_sequence:
+    #         if sequence[i] == sequence[i+3] == "H":
+    #             if option[i] != option[i+1] or option[i] == "forward":
+    #                 return False
+    #             else:
+    #                 i += 2
+    #         i += 1
+    #     return True
--- eiwitten/versie0.py
+++ eiwitten/versie0.py
@@ -15,39 +15,44 @@ def main():
     # checks whether current option is better than all previous ones
     options = Option(protein.length)
     field = Field(protein.length, protein.sequence)
-    best_fold = options.options[0]
+    last_fold_points = 0
 
-    new_ways = [["right"], ["left"], ["forward"]]
+    ways = [["right"], ["forward"]]
     #
     ### while(not_all_options):
     #
     # creates field and fold based on the protein and the current option
     for aminoacid in range(len(protein.sequence) - 3):
-        ways = deepcopy(new_ways)
-        # print("ways", ways)
         new_ways = []
+        all_ways = []
         for option in options.options:
             for route in range(len(ways)):
                 ways[route].append(option)
                 if field.fill_field(protein.sequence[:aminoacid + 4], ways[route]):
-                    new_ways.append(deepcopy(ways[route]))
-                    # print("new", new_ways)
+                    all_ways.append(deepcopy(ways[route]))
 
                     # check wether current fold is the best and remembers it if it is
-                    if fold_points(field) > best_fold_points:
-                        best_fold_points = fold_points(field)
-                        best_fold = deepcopy(ways[route])
-                        # print(best_fold)
+                    if int(options.points) > last_fold_points:
+                        new_ways.append(deepcopy(ways[route]))
+                        best_fold_points = int(options.points)
+
                 field.clear_field(protein.length)
                 field.x_cdn = protein.length - 1
                 field.y_cdn = protein.length
                 # print("before pop new", new_ways)
                 ways[route].pop()
+
+        if not len(new_ways) == 0:
+            ways = deepcopy(new_ways)
+        else:
+            ways = deepcopy(all_ways)
+
+        last_fold_points = best_fold_points
                 # print("after new", new_ways)
     # prints best_fold_points and best_fold and current field
-    print(best_fold_points - protein.errorpoint)
-    print(best_fold)
-    field.fill_field(protein.sequence, best_fold)
+    field.fill_field(protein.sequence, ways[-1])
+    print(fold_points(field) - protein.errorpoint)
+    print(ways[-1])
     for line in field.field:
         print(line)
     end = time.time()
@@ -67,11 +72,11 @@ def fold_points(field):
     for i in range(field.dimension):
        for j in range(field.dimension):
            if field.field[j][i] == "H":
-               for k in [[1,0],[0,-1]]:
+               for k in [[1,0],[0,1]]:
                    if field.field[j + k[0]][i + k[1]] == "H":
                        points += 1
            elif field.field[j][i] == "C":
-                for k in [[1,0],[0,-1]]:
+                for k in [[1,0],[0,1]]:
                     if field.field[j + k[0]][i + k[1]] == "C":
                         points += 5
     return(points)
