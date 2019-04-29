be10c64aeee642135457021f09ee4e38bb108767
Djenzenpan <jmppannekeet@gmail.com>
2019-04-29 14:59:30 +0200

    *

--- eiwitten/field.py
+++ eiwitten/field.py
@@ -3,23 +3,9 @@ class Field(object):
     def __init__(self, length, sequence):
         self.dimension = (length * 2)
         self.field = [["_"] * self.dimension for i in range(self.dimension)]
-        self.x_cdn = length - 1
-        self.y_cdn = length
-        self.right_ways = {'[0, 1]': [1,0], '[0, -1]': [-1,0], '[1, 0]': [0,-1], '[-1, 0]': [0,1]}
-        self.left_ways = {'[0, 1]': [-1,0], '[0, -1]': [1,0], '[1, 0]': [0,1], '[-1, 0]': [0,-1]}
-        self.forward_ways = {'[0, 1]': [0,1], '[0, -1]': [0,-1], '[1, 0]': [1,0], '[-1, 0]': [-1,0]}
-        self.coordinates = []
 
     """Fills field based on current option"""
     def fill_field(self, positions, sequence):
         for position, aminoacid in zip(positions, sequence):
-            print(position)
             (x, y) = position
             self.field[x][y] = aminoacid
-
-    def clear_field(self, length):
-        for x in range(len(self.field)):
-            for y in range(len(self.field)):
-                self.field[y][x] = "_"
-        # trying out coordinates
-        self.coordinates = []
