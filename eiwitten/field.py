25dbbbd96a6e93b26caae7c16fdc1ba0ceb54f84
Nils van Doorn <nilsvandoorn@sp-eduroam-223-124.wireless.uva.nl>
2019-04-30 18:19:29 +0200

    merge was verkeerd gegaan

--- eiwitten/field.py
+++ eiwitten/field.py
@@ -3,3 +3,9 @@ class Field(object):
     def __init__(self, length, sequence):
         self.dimension = (length * 2)
         self.field = [["_"] * self.dimension for i in range(self.dimension)]
+
+    """Fills field based on current option"""
+    def fill_field(self, positions, sequence):
+        for position, aminoacid in zip(positions, sequence):
+            (x, y) = position
+            self.field[x][y] = aminoacid
