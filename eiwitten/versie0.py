92e361e4f1555a828f2223fa5961d49688dcc51b
Thobio22 <thomas.verouden@hotmail.com>
2019-05-19 16:02:38 +0200

    merge tho

--- eiwitten/versie0.py
+++ eiwitten/versie0.py
@@ -22,7 +22,7 @@ def main():
 
     options = Option(protein.length)
     best_fold = options.options[0]
-    best_positions = [(0, 0), (0, 1)]
+    best_positions = []
 
     ways = [["right"], ["forward"]]
     last_fold_points = 0
@@ -33,7 +33,7 @@ def main():
 
     # creates fold based on the protein and the current option
     for aminoacid in range(len(protein.sequence) - 3):
-        round_points = []
+
         P1 = 0.7 / (1 + 200 * 0.65 ** (aminoacid + 4)) + 0.4
         P2 = 0.8 / (1 + 200 * 0.825 ** (aminoacid + 4)) + 0.5
 
