e011fb07cf7fb69c41f9c846309b1f9c9e6ade67
Djenzenpan <jmppannekeet@gmail.com>
2019-05-23 12:17:31 +0200

    Greedylookahead met functies apart

--- code/algoritmes/functions.py
+++ code/algoritmes/functions.py
@@ -40,10 +40,10 @@ def mirror(route):
     return True
 
 """Finds aminoacid positions in current fold and checks for bumps (2D)"""
-def amino_positions_2d(sequence, option):
+def amino_positions_2d(option):
     # initialises positions list and starting coordinates of protein
     positions = []
-    begin = ceil(len(sequence) // 2)
+    begin = ceil(len(option) // 2)
 
     # appends first two positions to positions list
     positions.append(tuple((begin, begin)))
@@ -101,119 +101,29 @@ def amino_positions_2d_hc(option):
     return positions
 
 """Finds aminoacid positions in current fold and checks for bumps (3D)"""
-def amino_positions_3d(sequence, option):
-
+def amino_positions_3d(option):
     # initialises positions list and starting coordinates of protein
     positions = []
-    begin = int(ceil(len(sequence) / 2))
+    begin = int(ceil(len(option) / 2))
 
     # appends first two positions to positions list
     positions.append(tuple((begin, begin, begin)))
     positions.append(tuple((begin, begin + 1, begin)))
 
-    # initialises starting x-, y- and z-coordinates and current direction
+    # initialises x-, y- and z-coordinates and current direction
     x, y, z = begin, begin + 1, begin
-    direction = "y_min"
-    directions = {'y_plus':{'right': [1,0,0,'x_min'], 'left': [-1,0,'x_plus'], 'forward': [0,-1]},
-                'x_plus':{'right': [0,-1,0,'y_min'], 'left': [0,1,0,'y_plus'], 'up': [0,0,1,'z_plus'], 'down': [0,0,-1,'z_min'], 'forward': [1,0,0,'x_plus']},
-                'x_min':{'right': [0,1,'y_plus'], 'left': [0,-1,'y_min'], 'forward': [1,0]},
-                'y_min':{'right': [-1,0,'x_plus'], 'left': [1,0,'x_min'], 'forward': [0,1]},
-                'z_plus':{'right': [-1,0,'x_plus'], 'left': [1,0,'x_min'], 'forward': [0,1]},
-                'z_min':{'right': [-1,0,'x_plus'], 'left': [1,0,'x_min'], 'forward': [0,1]}}
 
-    # loops over current option and finds aminoacid coordinates
+    direction = 'forward'
+    going_back = {'left':'right','right':'left','forward':'back','back':'forward','up':'down','down':'up'}
+    new_directions = {'left':[-1,0,0],'right':[1,0,0],'forward':[0,1,0],'back':[0,-1,0],'up':[0,0,1],'down':[0,0,-1],}
+
     for move in option:
-        if direction == "x_plus":
-            if move == "right":
-                y = y - 1
-                direction = "y_min"
-            elif move == "left":
-                y = y + 1
-                direction = "y_plus"
-            elif move == "up":
-                z = z + 1
-                direction = "z_plus"
-            elif move == "down":
-                z = z - 1
-                direction = "z_min"
-            elif move == "forward":
-                x = x + 1
-        elif direction == "y_min":
-            if move == "right":
-                x = x + 1
-                direction = "x_plus"
-            elif move == "left":
-                x = x - 1
-                direction = "x_min"
-            elif move == "up":
-                z = z + 1
-                direction = "z_plus"
-            elif move == "down":
-                z = z - 1
-                direction = "z_min"
-            elif move == "forward":
-                y = y + 1
-        elif direction == "y_plus":
-            if move == "right":
-                x = x - 1
-                direction = "x_min"
-            elif move == "left":
-                x = x + 1
-                direction = "x_plus"
-            elif move == "up":
-                z = z + 1
-                direction = "z_plus"
-            elif move == "down":
-                z = z - 1
-                direction = "z_min"
-            elif move == "forward":
-                y = y - 1
-        elif direction == "x_min":
-            if move == "right":
-                y = y + 1
-                direction = "y_plus"
-            elif move == "left":
-                y = y - 1
-                direction = "y_min"
-            elif move == "up":
-                z = z + 1
-                direction = "z_plus"
-            elif move == "down":
-                z = z - 1
-                direction = "z_min"
-            elif move == "forward":
-                x = x - 1
-        elif direction == "z_plus":
-            if move == "right":
-                x = x + 1
-                direction = "x_plus"
-            elif move == "left":
-                x = x - 1
-                direction = "x_min"
-            elif move == "up":
-                y = y + 1
-                direction = "y_plus"
-            elif move == "down":
-                y = y - 1
-                direction = "y_min"
-            elif move == "forward":
-                z = z + 1
-        elif direction == "z_min":
-            if move == "right":
-                x = x + 1
-                direction = "x_plus"
-            elif move == "left":
-                x = x - 1
-                direction = "x_min"
-            elif move == "up":
-                y = y + 1
-                direction = "y_plus"
-            elif move == "down":
-                y = y - 1
-                direction = "y_min"
-            elif move == "forward":
-                z = z - 1
-        # only appends coordinates if there are no bumps
+        if not going_back[move] == direction:
+            x += new_directions[move][0]
+            y += new_directions[move][1]
+            z += new_directions[move][2]
+            direction = move
+
         if tuple((x, y, z)) in positions:
             return False
         positions.append(tuple((x, y, z)))
--- code/algoritmes/greedylookahead.py
+++ code/algoritmes/greedylookahead.py
@@ -5,15 +5,13 @@ from path import Path
 import time
 from copy import deepcopy
 from math import ceil
+from functions import amino_positions_2d, amino_positions_3d, fold_points_2d, fold_points_3d, mirror
 import csv
 
 
 
 def main():
     """Asks for either 2D or 3D input, then uses the relevant code"""
-
-    # checks whether program is used correctly
-    check()
 
     # Determines program running time
     start = time.time()
@@ -41,8 +39,8 @@ def main():
             for route in ways:
                 for option in options.options:
                     route.append(option)
-                    if not options.mirror(route):
-                        coordinates_route = options.amino_positions(protein.sequence[:aminoacid + 4], route)
+                    if not mirror(route):
+                        coordinates_route = amino_positions_3d(route)
                         if coordinates_route:
                             pseudo_points = int(fold_points_3d(coordinates_route, protein.sequence) - protein.errorpoint[aminoacid + 3])
                             if aminoacid + 4 == protein.length:
@@ -79,8 +77,8 @@ def main():
             for route in ways:
                 for option in options.options_2D:
                     route.append(option)
-                    if not options.mirror(route):
-                        coordinates_route = amino_positions_2d(protein.sequence[:aminoacid + 4], route)
+                    if not mirror(route):
+                        coordinates_route = amino_positions_2d(route)
                         if coordinates_route:
                             pseudo_points = int(fold_points_2d(coordinates_route, protein.sequence) - protein.errorpoint[aminoacid + 3])
                             if aminoacid + 4 == protein.length:
@@ -128,101 +126,6 @@ def main():
         p.plot3Dfold(protein.sequence, best_fold_points)
     else:
         p.plotFold(protein.sequence, best_fold_points)
-
-
-# checks user input
-def check():
-    if len(argv) != 3:
-        exit("Usage: python versie0.py proteinsequence dimension")
-    for aminoacid in argv[1]:
-        if aminoacid != 'H' and aminoacid != 'P' and aminoacid != 'C':
-            exit("Protein sequence can only contain P, C and H")
-    if argv[2] != "2D" and argv[2] != "3D":
-            exit("Please add either 2D or 3D as dimension for folding, after the given proteinsequence")
-
-
-# checks the points scored by the current 3D fold
-def fold_points_3d(positions, sequence):
-    points = 0
-    HHHH = []
-    CCCC = []
-    for position, acid in zip(positions, sequence):
-        if acid == "H":
-            HHHH.append(position)
-        elif acid == "C":
-            CCCC.append(position)
-    for acid_position in HHHH:
-        for look_around in [[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],[0,0,1],[0,0,-1]]:
-            if (acid_position[0] + look_around[0], acid_position[1] + look_around[1], acid_position[2] + look_around[2]) in HHHH:
-                points += 1
-
-    for acid_position in CCCC:
-        for look_around in [[1,0,0],[-1,0,0],[0,1,0],[0,-1,0],[0,0,1],[0,0,-1]]:
-            if (acid_position[0] + look_around[0], acid_position[1] + look_around[1], acid_position[2] + look_around[2]) in CCCC:
-                points += 5
-            elif (acid_position[0] + look_around[0], acid_position[1] + look_around[1], acid_position[2] + look_around[2]) in HHHH:
-                points += 2
-    return points / 2
-
-
-# checks the points scored by the current 3D fold
-def fold_points_2d(positions, sequence):
-    points = 0
-    HHHH = []
-    CCCC = []
-    for position, acid in zip(positions, sequence):
-        if acid == "H":
-            HHHH.append(position)
-        elif acid == "C":
-            CCCC.append(position)
-    for acid_position in HHHH:
-        for look_around in [[1,0],[-1,0],[0,1],[0,-1]]:
-            if (acid_position[0] + look_around[0], acid_position[1] + look_around[1]) in HHHH:
-                points += 1
-
-    for acid_position in CCCC:
-        for look_around in [[1,0],[-1,0],[0,1],[0,-1]]:
-            if (acid_position[0] + look_around[0], acid_position[1] + look_around[1]) in CCCC:
-                points += 5
-            elif (acid_position[0] + look_around[0], acid_position[1] + look_around[1]) in HHHH:
-                points += 2
-    return points / 2
-
-
-# amino positions function for 2D
-def amino_positions_2d(sequence, option):
-    # initialises positions list and starting coordinates of protein
-    positions = []
-    begin = ceil(len(sequence) // 2)
-
-    # appends first two positions to positions list
-    positions.append(tuple((begin, begin)))
-    positions.append(tuple((begin + 1, begin)))
-
-    # initialises x-, y-coordinates and current direction
-    x, y = begin, begin + 1
-
-    # right, left and forward change with last direction
-    directions = {'y_min':{'right': [-1,0,'x_min'], 'left': [1,0,'x_plus'], 'forward': [0,1,'y_min']},
-                'x_plus':{'right': [0,1,'y_min'], 'left': [0,-1,'y_plus'], 'forward': [1,0,'x_plus']},
-                'x_min':{'right': [0,-1,'y_plus'], 'left': [0,1,'y_min'], 'forward': [-1,0,'x_min']},
-                'y_plus':{'right': [1,0,'x_plus'], 'left': [-1,0,'x_min'], 'forward': [0,-1,'y_plus']}}
-    direction = "y_min"
-
-    # loops over current option and appends aminoacid coordinates
-    # if there are no bumps
-    for move in option:
-        x += directions[direction][move][0]
-        y += directions[direction][move][1]
-        direction = directions[direction][move][2]
-
-        # only appends coordinates if there are no bumps
-        if tuple((y, x)) in positions:
-            return False
-        positions.append(tuple((y, x)))
-    return positions
-
-
 
 if __name__ == '__main__':
     main()
